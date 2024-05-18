6beea8b1cf6fde84cc593cf5a71e787ed006bf5f
Rahul Kumar <rk2909559@gmail.com>
2024-05-18 22:47:49 +0530

    First native code for practice

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text,Image, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Text> Hello india</Text>
+      
+      <Image source={require('./assets/mona.jpg')} />
       <StatusBar style="auto" />
     </View>
   );
